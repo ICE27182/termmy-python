afc4c5ab4034b62ea6844733e8fead6bb26e5789
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2025-07-02 21:07:08 +0200

    Added 'Colors', a factory class that generates 'Color' objects.

---
+++ termmy/colors/colors.py
@@ -0,0 +1,51 @@
+
+
+from .color import Color
+
+from random import random
+from random import seed as seed_
+
+class Colors:
+    """
+    Color constants
+    """
+    @staticmethod
+    def black() -> Color:
+        return Color(0.0, 0.0, 0.0)
+    @staticmethod
+    def white() -> Color:
+        return Color(1.0, 1.0, 1.0)
+    @staticmethod
+    def gray() -> Color:
+        return Color(0.5, 0.5, 0.5)
+
+    @staticmethod
+    def red() -> Color:
+        return Color(1.0, 0.0, 0.0)
+    @staticmethod
+    def green() -> Color:
+        return Color(0.0, 1.0, 0.0)
+    @staticmethod
+    def blue() -> Color:
+        return Color(0.0, 0.0, 1.0)
+
+    @staticmethod
+    def yellow() -> Color:
+        return Color(0.707, 0.707, 0.0)
+    @staticmethod
+    def magenta() -> Color:
+        return Color(0.707, 0.0, 0.707)
+    @staticmethod
+    def cyan() -> Color:
+        return Color(0.0, 0.707, 0.707)
+    
+    @staticmethod
+    def random_color(
+        seed: int | float | str | bytes | bytearray | None = None
+    ) -> Color:
+        seed_(seed)
+        return Color(random(), random(), random())
+
+    @staticmethod
+    def ice() -> Color:
+        return Color.from_ints(156, 220, 255)
