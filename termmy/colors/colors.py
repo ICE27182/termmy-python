ec0dffc67bc05386fcb981dfb145cb8832741f6b
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2025-07-23 17:00:33 +0800

    Added a few more colors

--- termmy/colors/colors.py
+++ termmy/colors/colors.py
@@ -9,6 +9,10 @@ from typing import final
 @final
 class Colors:
     @staticmethod
+    def ice() -> Color:
+        return Color.from_ints(156, 220, 255)
+    
+    @staticmethod
     def black() -> Color:
         return Color(0.0, 0.0, 0.0)
     @staticmethod
@@ -39,12 +43,45 @@ class Colors:
         return Color(0.0, 0.707, 0.707)
     
     @staticmethod
+    def orange() -> Color:
+        return Color(1.0, 0.667, 0.0)
+    @staticmethod
+    def lemon() -> Color:
+        return Color(1.0, 1.0, 0.0)
+    @staticmethod
+    def lime() -> Color:
+        return Color.from_ints(137, 243, 54)
+
+    @staticmethod
+    def pink() -> Color:
+        return Color(1.0, 0.5, 0.5)
+    @staticmethod
+    def salmon() -> Color:
+        return Color(1.0, 0.5, 0.4)
+    @staticmethod
+    def purple() -> Color:
+        return Color(0.5, 0.0, 0.5)
+    @staticmethod
+    def violet() -> Color:
+        return Color(0.5, 0.0, 1.0)
+    
+    @staticmethod
+    def indigo() -> Color:
+        return Color(0.0, 0.0, 0.5)
+    @staticmethod
+    def navy() -> Color:
+        return Color(0.0, 0.0, 0.25)
+    @staticmethod
+    def lapis_lazuli() -> Color:
+        return Color.from_ints(38, 97, 156)
+    @staticmethod
+    def sky_blue() -> Color:
+        return Color.from_ints(135, 206, 235)
+
+    @staticmethod
     def random_color(
         seed: int | float | str | bytes | bytearray | None = None
     ) -> Color:
         seed_(seed)
         return Color(random(), random(), random())
 
-    @staticmethod
-    def ice() -> Color:
-        return Color.from_ints(156, 220, 255)
