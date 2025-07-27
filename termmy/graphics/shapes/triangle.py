c92ee7a8ea68441f69eac2edb3985c90846fa429
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2025-07-27 11:55:27 +0800

    Changed vertices in a triangle from 'Vec2' to 'Vertex2'

--- termmy/graphics/shapes/triangle.py
+++ termmy/graphics/shapes/triangle.py
@@ -5,13 +5,13 @@ from dataclasses import dataclass
 from .shape import Shape
 from ..fills import Fill
 from ..stroke import Stroke
-from ...core import Vec2
+from ...core import Vertex2
 
 @dataclass(slots=True)
 class Triangle(Shape):
-    a: Vec2
-    b: Vec2
-    c: Vec2
+    a: Vertex2
+    b: Vertex2
+    c: Vertex2
     fill: Fill | None = None
     stroke: Stroke | None = None
     
