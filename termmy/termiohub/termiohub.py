7bac2012dbd19337969ee6556893cc28e8b9077d
ICE27182 <75039215+ICE27182@users.noreply.github.com>
2026-02-16 16:15:23 +0100

    Removed 'termmy/termiohub/safe_io.py'; Added 'KeyboardInput', 'MouseInput' and 'InputEvent'; Added a signature for 'TermIOHub.get_input_blocking'

--- termmy/termiohub/termiohub.py
+++ termmy/termiohub/termiohub.py
@@ -6,6 +6,47 @@ from collections import deque
 from typing import Self
 from dataclasses import dataclass, field
 from queue import Queue
+from enum import StrEnum, auto
+
+
+@dataclass(slots=True, frozen=True)
+class KeyboardInput:
+    class ModifierState(StrEnum):
+        YES = auto()
+        NO = auto()
+        UNKNOWN = auto()
+    key: str
+    raw: str
+    shift: ModifierState = ModifierState.UNKNOWN
+    control: ModifierState = ModifierState.UNKNOWN
+    command: ModifierState = ModifierState.UNKNOWN
+    alt: ModifierState = ModifierState.UNKNOWN
+    option: ModifierState = field(init=False)
+
+    def __post_init__(self):
+        object.__setattr__(self, "option", self.alt)
+
+@dataclass(slots=True, frozen=True)
+class MouseInput:
+    class ButtonState(StrEnum):
+        LEFT_DOWN = auto()
+        LEFT_RELEASE = auto()
+        RIGHT_DOWN = auto()
+        RIGHT_RELEASE = auto()
+        MIDDLE_DOWN = auto()
+        MIDDLE_RELEASE = auto()
+        SCROLL_UP = auto()
+        SCROLL_DOWN = auto()
+        NONE = auto()
+    x: int
+    y: int
+    button_state: ButtonState
+    raw: str
+
+@dataclass(slots=True, frozen=True)
+class InputEvent:
+    input: KeyboardInput | MouseInput
+    timestamp: float
 
 class TermIOHub:
     _active_instance: None
@@ -15,10 +56,11 @@ class TermIOHub:
     def __init__(self) -> None: ...
 
     def __enter__(self) -> Self: return self.start()
-    def __exit__(self, exc_type, exc_val, exc_tb): return self.close()
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None: return self.close()
 
     def start(self) -> Self: ...
-    def close(self): ...        
+    def close(self) -> None: ...        
 
-    def get_input(self) -> ...: ...
+    def get_input(self) -> InputEvent | None: ...
+    def get_input_blocking(self) -> InputEvent: ...
     def schedule_output(self, value: str): ...
