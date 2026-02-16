01d64c46c3e7877225d2e44f7ce0cbbd49c9f42c
ICE27182 <75039215+ICE27182@users.noreply.github.com>
2026-02-16 17:16:56 +0100

    Added '_TermModeContextManager' for cross-platform terminal environment setting

--- termmy/termiohub/termiohub.py
+++ termmy/termiohub/termiohub.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 
 from threading import Thread, RLock
 from collections import deque
-from typing import Self
+from typing import Self, override
 from dataclasses import dataclass, field
 from queue import Queue
 from enum import StrEnum, auto
+
+from .term_mode import _TermModeContextManager
 
 
 @dataclass(slots=True, frozen=True)
@@ -48,18 +50,20 @@ class InputEvent:
     input: KeyboardInput | MouseInput
     timestamp: float
 
-class TermIOHub:
+class TermIOHub(_TermModeContextManager):
     _active_instance: None
     _inputs: deque[str] # One producer, multiple consumers
     _outputs: Queue[str] # Multiple producers, one consumer
 
     def __init__(self) -> None: ...
 
-    def __enter__(self) -> Self: return self.start()
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None: return self.close()
-
-    def start(self) -> Self: ...
-    def close(self) -> None: ...        
+    @override
+    def __enter__(self) -> Self:
+        super().__enter__()
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None: 
+        super().__exit__(exc_type, exc_val, exc_tb)
 
     def get_input(self) -> InputEvent | None: ...
     def get_input_blocking(self) -> InputEvent: ...
