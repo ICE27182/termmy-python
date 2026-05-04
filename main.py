e282cb77cb0a1ce0edecacbe5dba44c583b32941
ICE27182 <ice27182@ICE27182s-MacBook-Air.local>
2026-05-04 13:57:25 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from time import sleep, time
 from shutil import get_terminal_size
+from subprocess import run
 
 from termmy.termiohub import TermIOHub, constants
 from termmy.termiohub import InputEvent, KeyboardInput, MouseInput
@@ -10,12 +11,15 @@ from termmy.colors import Colors
 from termmy.core import Transform
 from termmy.rendering.basic_rendering_functions import rasterize_pixel_point
 
+run("clear", check=True)
+run("clear", check=True)
+
 SUPER = False
 
 WIDTH, HEIGHT = 80, 48
-WIDTH, HEIGHT = get_terminal_size()
-WIDTH //= 2
-if SUPER: WIDTH, HEIGHT = WIDTH * 2, HEIGHT * 2
+# WIDTH, HEIGHT = get_terminal_size()
+# WIDTH, HEIGHT = WIDTH // 2, HEIGHT - 2
+# if SUPER: WIDTH, HEIGHT = WIDTH * 2, HEIGHT * 2
 fb = FrameBuffer.from_size(WIDTH, HEIGHT)
 
 oval = Circle(
@@ -31,11 +35,11 @@ pline = PixelLine(
 
 point = (0.0, 0.0)
 
+def pt(arg): print(arg, end='')
 
-with TermIOHub() as iohub:
-    def print(arg, end=''): iohub.output(arg)
+with TermIOHub(concurrent_output=False) as iohub:
+    def pt(arg): iohub.output(arg)
 # if True:
-    print('\r\n', end='')
     SHOW_INPUT = 25
     show_input = SHOW_INPUT
     input = ''
@@ -47,29 +51,35 @@ with TermIOHub() as iohub:
         oval.render(fb)
         pline.render(fb)
         rasterize_pixel_point(*point, Colors.red(), fb.color_buffer)
+        rasterize_pixel_point(1, 1, Colors.cyan() if int(time()) & 1 else Colors.blue(), fb.color_buffer)
         oval.transform.rotate_z_by(0.02)
         
         if SUPER:
-            print(ansi24_4x(fb.color_buffer.width, fb.color_buffer.height, fb.color_buffer.data), end='')
+            pt(ansi24_4x(fb.color_buffer.width, fb.color_buffer.height, fb.color_buffer.data))
         else:
-            print(ansi24(fb.color_buffer.width, fb.color_buffer.height, fb.color_buffer.data), end='')
-        print(f"\r\n{time()}", end='')
+            pt(ansi24(fb.color_buffer.width, fb.color_buffer.height, fb.color_buffer.data))
+        pt(f"\r\n{time()}")
         
-        input_event = iohub.get_input(10)
+        input_event = iohub.get_input()
         if input_event is not None:
             input = input_event.input
             if isinstance(input, KeyboardInput):
                 if (input.raw == b"Q"
                     or input == constants.PredefinedKeys.C_CTRL.value):
-                    print(f"\033[{HEIGHT+6}B\r\n", end='')
+                    pt(f"\033[{HEIGHT+6}B\r\n")
                     break
             elif isinstance(input, MouseInput):
                 if input.button == MouseInput.Button.LEFT:
-                    x, y = input.x / 2, input.y
+                    # You can drag the oval around with the left mouse button
+                    x, y = input.x, input.y
+                    if SUPER:
+                        y *= 2
+                    else:
+                        x /= 2
                     if fb.entity_buffer.get_entity(int(x), y) is oval:
                         oval.transform.translate_to(x, y, 0.0)
                     point = (x, y)
         
-        print(f"\033[{HEIGHT // 2 if SUPER else HEIGHT}A", end='')
-        sleep(1 / 60)
+        pt(f"\033[{HEIGHT // 2 if SUPER else HEIGHT}A\r")
+        sleep(1 / 1000)
         
